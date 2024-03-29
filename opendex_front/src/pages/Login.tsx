bef59d5f03d24afb5702d91e10d55869fd50edce
L4p1s <124094769+PommePain@users.noreply.github.com>
2024-03-29 14:23:03 +0100

    Base files for front (#7)
    Review and validation down by Spyko

---
+++ opendex_front/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from 'react';
+import "../css/login.css";
+
+const Login: FunctionComponent = () => {
+  return (
+    <div className="Login">
+        Se connecter
+    </div>
+  );
+}
+
+export default Login;
